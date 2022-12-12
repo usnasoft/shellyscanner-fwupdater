a1e554b6763ddad4bd0bb713d0065d3a86e17f7f
aflaccomio <aflaccomio@gmail.com>
2022-12-12 22:06:42 +0100

    v1.0.0

--- src/main/java/it/usna/shellyscan/fwupdate/FWUpdater.java
+++ src/main/java/it/usna/shellyscan/fwupdate/FWUpdater.java
@@ -1,62 +1,79 @@
 package it.usna.shellyscan.fwupdate;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.net.ConnectException;
 import java.net.HttpURLConnection;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.URL;
+import java.net.URLConnection;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
+@SuppressWarnings("restriction")
 public class FWUpdater {
 	private final static int PORT = 8000;
 	private final static String CONTEXT = "shellyfw";
 
 	private final static String IPV4_REGEX = "^((0|1\\d?\\d?|2[0-4]?\\d?|25[0-5]?|[3-9]\\d?)\\.){3}(0|1\\d?\\d?|2[0-4]?\\d?|25[0-5]?|[3-9]\\d?)$";
 
-	public static void main(String ... args) throws IOException {
-		if(args.length != 2) {
+	public static void main(String... args) throws IOException {
+		if (args.length != 2) {
 			usage();
 			System.exit(1);
 		}
 		String shellyIP = args[0];
-		if(shellyIP.matches(IPV4_REGEX) == false) {
+		if (shellyIP.matches(IPV4_REGEX) == false) {
 			System.out.println("invalid IP.");
 			usage();
 			System.exit(2);
 		}
 		File file = new File(args[1]);
-		if(file.exists() == false) {
+		if (file.exists() == false) {
 			System.out.println("file not found.");
 			usage();
 			System.exit(3);
 		}
 
+		HttpServer server = null;
 		try {
 			InetSocketAddress socket = new InetSocketAddress(InetAddress.getLocalHost(), PORT);
-			HttpServer server = HttpServer.create(socket, 0);
+			server = HttpServer.create(socket, 0);
 			server.createContext("/" + CONTEXT, new MyHandler(file));
 			server.setExecutor(null); // creates a default executor
 			server.start();
 
-			System.out.println("Server is ready at " + socket.getAddress().getHostAddress());
-			System.out.println("Updating firmware ...\nDO NOT close terminal.");
-			//		System.out.println("Write following link into browser: http://" + socket.getAddress().getHostAddress() + ":" + PORT + "/" + CONTEXT);
+			System.out.println("Temporary server ready at address " + socket.getAddress().getHostAddress());
+			System.out.println("Updating firmware ...");
 			final URL url = new URL("http://" + shellyIP + "/ota?url=http://" + socket.getAddress().getHostAddress() + ":" + PORT + "/" + CONTEXT);
-			System.out.println(url.getContent());
-			server.stop(1000);
-			System.out.println("Process end");
+			URLConnection urlcon = url.openConnection();
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(urlcon.getInputStream()))) {
+				System.out.println("Device response to command:");
+				String out;
+				while ((out = br.readLine()) != null) {
+					System.out.println(out);
+				}
+			}
+			System.out.println("Press ctrl^C when firmware update is complete.\nDO NOT close terminal before");
 
-//			System.out.println("Press ctrl^C when firmware update is concluded.\nDo not close terminal before.");
-
-		} catch(Exception e) {
+		} catch (ConnectException e) {
+			System.out.println("Shelly device at " + shellyIP + " is not responding");
+			System.exit(4);
+		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+//			if (server != null) {
+//				server.stop(5000);
+//				System.exit(0);
+//			}
 		}
 	}
 
@@ -83,8 +100,6 @@ public class FWUpdater {
 	}
 
 	private static void usage() {
-		System.out.println("Usage: java -jar ... <shelly ip> <firmware file>");
+		System.out.println("Usage: java -jar shellyscanner-fwupdater-1.0.0.jar <shelly ip> <firmware file>");
 	}
 }
-
-// http://<ip>/ota?url=http:// ...
